e5cb1a83bb9517b990c82ab9a493cd3f0b76671b
StalinSamuelRaja <srstalinsr@gmail.com>
2023-08-31 17:32:07 +0530

    initial setup

---
+++ function.js
@@ -0,0 +1,15 @@
+// name();
+// function name(){
+//     console.log("Stalin")
+// }
+
+
+function greetMe(name){
+    return `hey ${name} welcome to guvi `;
+}
+
+function executeFunction(func,arg){
+    console.log(func(arg));
+}
+executeFunction(greetMe,'stalin' )
+
---
+++ task.js
@@ -0,0 +1,17 @@
+let xhr = new XMLHttpRequest();
+console.log(xhr);
+xhr.open("GET",'https://restcountries.com/v3.1/all')
+xhr.send();
+xhr.onload=function(){
+    const data=JSON.parse(xhr.response);
+    console.log(data);
+    console.log(xhr.status);
+    for(let i=0;i<data.length;i++){
+        console.log("1 countries_name:",data [i].name.common);
+        console.log("2 flag:",data [i].flags.png);
+        console.log("3 region:",data [i].region);
+        console.log("4 sub_region:",data [i].subregion);
+        console.log("5 population:",data [i].population);
+        //commit
+    }
+};l
